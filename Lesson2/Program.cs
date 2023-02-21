8d891ef9e48c62ad2c41e08cc6ae7b9209554127
Nikita <welcometoheeeell@gmail.com>
2023-02-21 19:59:13 +0300

    2 homework

---
+++ Lesson2/Program.cs
@@ -0,0 +1,55 @@
+﻿     //1 zadacha
+//void PrintSecondDigit(string? str)
+//{
+//    Console.WriteLine(str?[1]);
+//}
+//string? result = Console.ReadLine();
+//PrintSecondDigit(result);
+
+
+     //2 zadacha
+/* while(true) {
+void PrintThirdDigit(string? str)
+{
+    if (str?.Length < 3){
+        Console.WriteLine("Третьей цифры нет");
+
+    }else
+    Console.WriteLine(str?[2]);
+}
+string? result = Console.ReadLine();
+PrintThirdDigit(result);
+}
+*/
+     //3 zadacha
+
+void PrintDay(int day)
+{
+    switch(day){
+        case 1: if(day == (int)Days.Monday)
+            Console.WriteLine("Monday");
+            break;
+        case 2: if(day == (int)Days.Tuesday)
+            Console.WriteLine("Tuesday");
+            break;
+        case 3: if(day == (int)Days.Wednesday)
+            Console.WriteLine("Wednesday");
+            break;
+        case 4: if(day == (int)Days.Thursday)
+            Console.WriteLine("Thursday");
+            break;
+        case 5: if(day == (int)Days.Friday)
+            Console.WriteLine("Friday");
+            break;
+        case 6: if(day == (int)Days.Saturday)
+            Console.WriteLine("Saturday");
+            break;
+        case 7: if(day == (int)Days.Sunday)
+            Console.WriteLine("Sunday");
+            break;
+
+    }
+    }
+int day = Convert.ToInt32(Console.ReadLine());
+PrintDay(day);
+enum Days{Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -12,7 +12,7 @@
 //int num1 = Convert.ToInt32(Console.ReadLine());
 //int num2 = Convert.ToInt32(Console.ReadLine());
 //int num3 = Convert.ToInt32(Console.ReadLine());
-//if (num1 > num2 & num1 >num3) {Console.WriteLine(num1);}
+//if (num1 > num2 && num1 >num3) {Console.WriteLine(num1);}
 //else if (num2 > num1 & num2 > num3) {Console.WriteLine(num2);}
 //else {Console.WriteLine(num3);}
   //Zadacha3
