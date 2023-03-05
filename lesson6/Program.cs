95ce75da2bfcc3fcb62a747cba265f3b71c4a62f
Danila <danik.hoo@mail.ru>
2023-03-05 17:54:49 +0300

    homework

---
+++ lesson6/Program.cs
@@ -0,0 +1,27 @@
+﻿   /* int [] arr(int count){
+    int [] array = new int [count];
+    for(int i = 0; i < count; i++){
+        Console.WriteLine("Введите число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+    return array;
+}
+void PrintArray(int [] array){
+    foreach(int newarray in array){
+        Console.Write(newarray +" ");
+}
+}
+void PositiveNumber(int [] array){
+    int PosNum = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i] > 0){
+            PosNum = PosNum + 1;
+        }
+    }
+    Console.WriteLine("Количество положительных чисел ="+" "+ PosNum);
+}
+Console.WriteLine("Введите количество чисел");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = arr(N);
+PrintArray(array);
+PositiveNumber(array);*/
